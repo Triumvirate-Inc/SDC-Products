414130e63868355ed48ec11edf25cd67bbe85f21
ShubhamSaini1585 <shubham.saini@knoldus.com>
2022-03-14 10:19:18 +0530

    Removed Commented Code as well as Removed node_modeules

--- server/app.js
+++ server/app.js
@@ -20,36 +20,23 @@ const sendResponse = function(err, data, res) {
 
 // Get Products
 app.get('/shopdata/products/', (req, res) => {
-  // console.log('Params :', req.params);
-  // res.send('Hello');
   db.getAllProducts(
     (err, data) => sendResponse(err, data, res));
 });
 
 //Get Single Product
 app.get(`/shopdata/product/:product_id`, (req, res) => {
-  // res.send('Hello');
   const params = { product_id: req.params.product_id };
-  // console.log('REQ :', params);
   db.getProduct(params.product_id,
     (err, data) => sendResponse(err, data, res));
 });
 
 //Get Single Product Style
 app.get('/shopdata/product/:product_id/styles', (req, res) => {
-  // res.send('Hello');
   const params = { product_id: req.params.product_id };
   console.log(params.product_id);
   db.getStyles(params.product_id,
     (err, data) => sendResponse(err, data, res));
 });
 
-// //Get Related Products
-// app.get('/shopdata/products/:product_id/related', (req, res) => {
-//   const params = { product_id: req.query.product_id };
-//   db.getRelatedItems(params,
-//     (err, data) => sendResponse(err, data, res));
-// });
-
-// Listening for requests on the PORT
 module.exports = app;
